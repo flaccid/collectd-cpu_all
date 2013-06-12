52f569a04742b46d0cd8c729b25fb832da5c1d4c
Chris Fordham <chris.fordham@rightscale.com>
2013-06-11 17:06:56 -0700

    Remove un-needed comments and counter code.

--- collectd_cpu_all.py
+++ collectd_cpu_all.py
@@ -13,18 +13,12 @@ import psutil
 import time
 import sys
 
-x = 1
-
 while True:
 	cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
 	cpu_avg = sum(cpu_usage) / float(len(cpu_usage))
 	timenow = str(time.time()).split('.')[0]
 
-	#print 'CPU cores count: '+str(len(cpu_usage))
-	#print cpu_avg
-
 	print 'PUTVAL '+sys.argv[1]+'/cpu_all/gauge-average '+timenow+':'+str(cpu_avg)
 	print 'PUTVAL '+sys.argv[1]+'/cpu_all/gauge-cores '+timenow+':'+str(len(cpu_usage))
 
-	x += 1
 	time.sleep(float(sys.argv[2]))
